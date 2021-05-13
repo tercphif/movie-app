4acb514c573c415ee76106991abded4b36314765
Terrance Phifer <tercphif@gmail.com>
2021-05-13 17:53:12 -0500

    Started project
    Created the app and added components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieListComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule
